c6c8956c11fe0144a79bd6a8522636200f83ea26
Amund Tenstad <github@amund.io>
2022-06-07 13:01:50 +0200

    fix: bind ip (#11)

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ async fn main() -> std::io::Result<()> {
             .service(ready)
             .service(get_score)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
